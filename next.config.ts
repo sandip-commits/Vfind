48a1694bc66d579dabc6d86da15077cf75e28776
Shawn <karkisandip064@gmail.com>
2025-09-21 16:40:35 +0545

    Fix Next.js image config and suspense errors

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "x8ki-letl-twmt.n7.xano.io", 
+        hostname: "x76o-gnx4-xrav.a2.xano.io",
+        pathname: "/**",
       },
-       {
+      {
         protocol: "https",
         hostname: "images.unsplash.com",
-      },
-       {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        port: '',
-        pathname: '/**',
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'dev-to-uploads.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+        pathname: "/**",
       },
-      
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   },
 };
